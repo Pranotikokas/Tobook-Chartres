e2487a0d08ed46cb9899387547954f725bc7bc2b
PTony <jokr.01@gmail.com>
2016-06-07 14:50:50 +0200

    RatingBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new UserBundle\UserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new CommentBundle\CommentBundle(),
+            new RatingBundle\RatingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
